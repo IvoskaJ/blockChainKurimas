f064a3f812ec9207dd7bca490f52361aacf9c4dc
IvoskaJ <78845735+IvoskaJ@users.noreply.github.com>
2021-11-11 22:42:36 +0200

    3rd commit

--- User.h
+++ User.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <ctime>
 #include <cmath>
+#include <random>
 #include "sha256.cpp"
 
 using namespace std;
@@ -17,61 +18,46 @@ class user {
 				"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 				"abcdefghijklmnopqrstuvwxyz"
 				"!@#$%^&*()_+=/|<>?:{},.;'\[]";
-
 			for (int i = 0; i < len; ++i) {
 				s += alphanum[rand() % (sizeof(alphanum) - 1)];
 			}
-
 			return sha256(s);
 		}
-
 		string  gen_randomForName(int len) {
 			string s;
 			static const char alphanum[] =
 				"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 				"abcdefghijklmnopqrstuvwxyz";
-
 			for (int i = 0; i < len; ++i) {
 				s += alphanum[rand() % (sizeof(alphanum) - 1)];
 			}
-
 			return s;
 		}
-
 		string generatePublicKey() {
 			string publicKey;
-
-			publicKey = gen_random(16);
-
+			publicKey = gen_random(1000);
 			return publicKey;
 		}
-
 		string generatePrivateKey() {
 			string privateKey;
-
-			privateKey = gen_random(32);
-
+			privateKey = gen_random(1000);
 			return privateKey;
 		}
-
 		string generateName() {
 			string name;
-
 			name = gen_randomForName(8);
-
 			return name;
 		}
-
-		void setPrivateKey(string input) { privateKey = input; }
-		void setPublicKey(string input) { publicKey = input; }
-		void setName(string input) { name = input; }
-		void createBalance(double balance) { balance = fabs(rand() % 1000); }
+		void setPrivateKey(string input) { this->privateKey = input; }
+		void setPublicKey(string input) { this->publicKey = input; }
+		void setName(string input) { this->name = input; }
+		void createBalance(int input) { this->balance = fabs(rand()); }
 	private:
 		string publicKey = "";
 		string name = "";
 		string privateKey="";
 		string getPrivateKey() { return privateKey; }
-		double balance = 0;
+		int balance = 0;
 	public:
 		user() {
 			setPrivateKey(generatePrivateKey());
@@ -79,11 +65,16 @@ class user {
 			setName(generateName());
 			createBalance(balance);
 		}
-		double setBalance(double amountTraded) {
+		int setBalance(int amountTraded) {
             this->balance = this->balance - amountTraded;
             return this->balance;
 		}
-		double getBalance(){
+		int rewardMiner(int difficultyTarget){
+		    int amountRewarded = 3 * difficultyTarget;
+            this->balance = this->balance + amountRewarded;
+            return this->balance;
+		}
+		int getBalance(){
 		    return balance;
         }
 		string getPublicKey() {
@@ -99,7 +90,7 @@ class transaction{
      string timestamp;
      user user1;
      user user2;
-     double amountTraded;
+     int amountTraded;
  public:
      user setUser1(user user){
         this->user1 = user;
@@ -109,25 +100,27 @@ class transaction{
         this->user2 = user;
         return this->user2;
      }
-     double setAmountTraded(double amount){
+     int setAmountTraded(int amount){
         this->amountTraded = amount;
         return this->amountTraded;
      }
-     string getUser1PubKey() {return this->user1.getPublicKey(); }
-     string getUser2PubKey() {return this->user2.getPublicKey(); }
-     string getTimestamp() {return this->timestamp;}
-     double getAmountTraded() {return this->amountTraded;}
+     string getUser1PubKey() {
+         return this->user1.getPublicKey();
+     }
+     string getUser2PubKey() {
+         return this->user2.getPublicKey();
+     }
+     string getTimestamp() {
+         return this->timestamp;
+     }
+     int getAmountTraded() {
+         return this->amountTraded;
+     }
 
-    void performTransaction(double amountTraded){
+    void performTransaction(int amountTraded){
         user1.setBalance(this->amountTraded);
         user2.setBalance(-(this->amountTraded));
         time_t now = time(0);
         this->timestamp = ctime(&now);
     }
 };
-
-vector<user> generateUsers() {
-	vector<user> generatedUsers;
-	generatedUsers.resize(1000);
-	return generatedUsers;
-}
--- block.h
+++ block.h
@@ -1,59 +1,133 @@
 #pragma once
 #include "User.h"
+#include <fstream>
 
 using namespace std;
 
-class blockGenesis {
+class block {
 	private:
+    string prevBlockHash = "this is the genesis block";
+    string merkelTreeHash;
 	string blockHash;
 	string timeStamp;
+	int index;
 	int nonce;
 	int version;
 	int difficultyTarget;
-    vector<transaction> transactionList;
+	double durationTakenToMine;
 	protected:
-
+	int createNonce(int difficultyTarget){
+	    this->nonce = fabs(rand()) / pow(2, difficultyTarget);
+        return this->nonce;
+	}
 	public:
-	blockGenesis() {
-		nonce = fabs(rand());
-		blockHash = sha256(to_string(nonce));
+	block() {
+		blockHash = sha256((to_string(nonce)) + this->prevBlockHash + (to_string(this->difficultyTarget)) + (to_string(this->version)) + this->timeStamp);
+		merkelTreeHash = sha256(blockHash + prevBlockHash);
 		time_t now = time(0);
 		timeStamp = ctime(&now);
-		version = 0.1;
-		difficultyTarget = 0;
+		version = 1;
+		index = 0;
+		difficultyTarget = 1;
+		nonce = createNonce(difficultyTarget);
 	}
-	string getBlockHash() {
-		return blockHash;
+	string setTimestamp(){
+        time_t now = time(0);
+        this->timeStamp = ctime(&now);
+        return this->timeStamp;
 	}
-	string getTimeStamp() {
-		return timeStamp;
-	}
-	int getVersion() {
-		return version;
-	}
-    void getTransaction(int index){
-        cout << "user1: " << transactionList.at(index).getUser1PubKey() << endl;
-        cout << "user2: " << transactionList.at(index).getUser2PubKey() << endl;
-        cout << "amount sent from user1 to user2" << transactionList.at(index).getAmountTraded() << endl;
-        cout << "time tranaction occured at: " << transactionList.at(index).getTimestamp() << endl;
-    }
-
-    vector<transaction> addTransactionToBlock(transaction transaction){
-        this->transactionList.push_back(transaction);
-        return this->transactionList;
-    }
-};
-
-blockGenesis genesis;
-
-class block: public blockGenesis{
-    private:
-    string prevBlockHash;
-    protected:
     string takePrevBlockHash(block block){
         this->prevBlockHash = block.getBlockHash();
         return this->prevBlockHash;
+    }
+    string getMerkelTreeHash(){
+        return this->merkelTreeHash;
+    }
+    string calculateMerkellTreeHash(block prevblock){
+       this->merkelTreeHash = sha256(getBlockHash() + getBlockHash() + getBlockHash() + getPrevBlockHash());
+       return this->merkelTreeHash;
+    }
+
+    string getPrevBlockHash(){
+        return this->prevBlockHash;
+    }
+	string getBlockHash() {
+		return this->blockHash;
+	}
+	string getTimeStamp() {
+		return this->timeStamp;
+	}
+	int getNonce(){
+        return this->nonce;
+	}
+	int getVersion() {
+		return this->version;
+	}
+	double getDuration(){
+        return this->durationTakenToMine;
+	}
+	double setDuration(auto duration){
+        durationTakenToMine = duration;
+        return this->durationTakenToMine;
+	}
+	int getIndex(){
+        return this->index;
+	}
+	int setIndex(int input){
+	    this->index = input;
+	    return this->index;
+	}
+	int getDifficultyTarget(){
+        return this->difficultyTarget;
+	}
+
+    int calculateDifficultyTarget(int index){
+        if(index == pow(2, this->difficultyTarget)){
+            this->difficultyTarget=this->difficultyTarget+1;
         }
-    public:
-    string getPrevBlockHash(){return this->prevBlockHash;}
+        return this->difficultyTarget;
+    }
+
+	vector<transaction> transactionList;
+
+    vector<transaction> addTransactionToBlock(transaction newTransaction){
+        this->transactionList.push_back(newTransaction);
+        return this->transactionList;
+    }
+
+    void getTransaction(int index){
+        cout << "transaction " << index << endl;
+        cout << "--------------------------------" << endl;
+        cout << "user1: " << transactionList.at(index).getUser1PubKey() << endl;
+        cout << "--------------------------------" << endl;
+        cout << "user2: " << transactionList.at(index).getUser2PubKey() << endl;
+        cout << "--------------------------------" << endl;
+        cout << "amount sent from user1 to user2: " << transactionList.at(index).getAmountTraded() << endl;
+        cout << "--------------------------------" << endl;
+        cout << "time tranaction occured at: " << transactionList.at(index).getTimestamp() << endl;
+        cout << "--------------------------------" << endl;
+    }
+
+    void getEntireTransactionList(){
+        for(int i =0; i<this->transactionList.size(); i++){
+            getTransaction(i);
+        }
+    }
+    void printListOfTransactions(){
+        ofstream fr(getBlockHash() + ".txt");
+        for(int i=0; i<transactionList.size(); i++){
+            fr << "transaction " << i << endl;
+            fr << "--------------------------------" << endl;
+            fr << "user1: " << transactionList.at(i).getUser1PubKey() << endl;
+            fr << "--------------------------------" << endl;
+            fr << "user2: " << transactionList.at(i).getUser2PubKey() << endl;
+            fr << "--------------------------------" << endl;
+            fr << "amount sent from user1 to user2: " << transactionList.at(i).getAmountTraded() << endl;
+            fr << "--------------------------------" << endl;
+            fr << "time tranaction occured at: " << transactionList.at(i).getTimestamp() << endl;
+            fr << "--------------------------------" << endl;
+        }
+        fr.close();
+    }
+
 };
